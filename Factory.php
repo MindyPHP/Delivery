259dbdee271ff84cbd5e46d0be86ca0c5b2566db
Falaleev Maxim <max@studio107.ru>
2018-01-24 16:56:08 +0300

    Added factory

---
+++ Factory.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Studio 107 (c) 2018 Maxim Falaleev
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mindy\Bundle\RiseBundle\Delivery;
+
+use Mindy\Delivery\Service\DeliveryServiceInterface;
+
+class Factory
+{
+    /**
+     * @var DeliveryServiceInterface[]
+     */
+    protected $services = [];
+
+    /**
+     * @param string                   $code
+     * @param DeliveryServiceInterface $service
+     */
+    public function registerService(string $code, DeliveryServiceInterface $service)
+    {
+        $this->services[$code] = $service;
+    }
+
+    /**
+     * @param $code
+     *
+     * @return DeliveryServiceInterface|null
+     */
+    public function getService($code)
+    {
+        if (isset($this->services[$code])) {
+            return $this->services[$code];
+        }
+
+        return null;
+    }
+
+    public function getServices(): array
+    {
+        return $this->services;
+    }
+}
